eb1f9b16ef7e07c8b3ab0f56a05003783697c7ad
Arslan_Maqsood <atharlaique15@gmail.com>
2020-01-28 12:42:40 +0500

    Fake Auth Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
-
 //Importing Component
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
---
+++ src/app/auth.service.ts
@@ -0,0 +1,19 @@
+export class AuthService{
+    loggedIn=false;
+    Authuntecate(){
+        const promise=new Promise(
+            (resolve, reject)=>{
+                setTimeout(()=>{
+                    resolve( this.loggedIn);
+                },8000)
+            }
+        )
+       return promise;
+    }
+    loggIn(){
+        this.loggedIn=true;
+    }
+    logout(){
+        this.loggedIn=false;
+    }
+}
